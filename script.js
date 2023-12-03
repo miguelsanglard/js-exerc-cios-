0821b02cae786c90d58248402ddc912ed6510240
Miguel Sanglard <miguelsanglard@gmail.com>
2023-12-03 02:35:35 -0300

    tipos de dados

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var nome = "miguel";
-var idade = 17;
-var comidafavorita;
-comidafavorita = "comida";
+// Declare uma variável contendo uma string
+var teste = "string";
+// Declare uma variável contendo um número dentro de uma string
+var teste2 = "1";
 
-var teste1, teste2, teste3, teste4, teste5;
-console.log(nome, idade, comidafavorita);
+// Declare uma variável com a sua idade
+var idade = "17";
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "miguel";
+var sobrenome = "sanglard";
+junto = nome + sobrenome;
+console.log(junto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
